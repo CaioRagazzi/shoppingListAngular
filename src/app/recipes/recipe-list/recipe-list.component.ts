cdaad0dd8fbe7e6343d303b31801be9d53beaa1b
Caio <ca.ragazzi@gmail.com>
2020-03-23 19:55:36 -0300

    continuação curso

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes.model';
 
 @Component({
@@ -8,13 +8,18 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simple a test', 'https://www.skinnytaste.com/wp-content/uploads/2009/02/turkey-meatloaf-8.jpg'),
-    new Recipe('A Second Recipe','This is another test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
+    new Recipe('A Test Recipe','This is simple a test', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg'),
+    new Recipe('A Second Recipe','This is another test', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/slow-cooker-recipes-1574787490.jpg?crop=0.753xw:1.00xh;0.224xw,0&resize=640:*'),
   ];
+  @Output() clickedRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickedItem(recipe: Recipe){
+    this.clickedRecipe.emit(recipe);
   }
 
 }
